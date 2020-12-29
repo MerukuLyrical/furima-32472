519b72f97c32213ee3dada8c8d9355370af536be
MerukuLyrical <merukurip@gmail.com>
2020-12-29 17:16:34 +0900

    コードレビュー修正版2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name                  { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 8) }
+    password              { 'hoge1111' }
     password_confirmation { password }
     last_name               { '試験' }
     first_name              { '試験' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -96,8 +96,14 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
-    it 'passwordは英数字混合でなければ登録できない' do
+    it 'passwordは英字のみでは登録できない' do
       @user.password = 'aaaaaa'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password には英字と数字を混合して設定してください')
+    end
+    it 'passwordは数字のみでは登録できない' do
+      @user.password = '111111'
       @user.password_confirmation = @user.password
       @user.valid?
       expect(@user.errors.full_messages).to include('Password には英字と数字を混合して設定してください')
