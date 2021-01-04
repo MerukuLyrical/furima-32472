6f26a8aeb85666926085d42625be663c1ffad7ad
MerukuLyrical <merukurip@gmail.com>
2021-01-04 10:43:35 +0900

    ActiveHash導入•実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area
+  belongs_to :category
+  belongs_to :delivary_fee
+  belongs_to :need_day
+  belongs_to :status
 
+  
+
+  validates :area_id,         numericality: { other_than: 1 } 
+  validates :category_id,     numericality: { other_than: 1 } 
+  validates :delivary_fee_id, numericality: { other_than: 1 } 
+  validates :need_day_id,     numericality: { other_than: 1 } 
+  validates :status_id,       numericality: { other_than: 1 } 
 end
---
+++ spec/factories/need_days.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :need_day do
+    
+  end
+end
