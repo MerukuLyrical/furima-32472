89fb996393a7c2493291c46afcb1012ab76cfcff
MerukuLyrical <merukurip@gmail.com>
2021-01-10 21:16:13 +0900

    クレジットカード購入機能実装

--- app/controllers/buying_logs_controller.rb
+++ app/controllers/buying_logs_controller.rb
@@ -5,12 +5,14 @@ class BuyingLogsController < ApplicationController
     @buying = Buying.new
   end
 
-
+# アイテムの値段を取り出す記述を行う
 
   def create
     @item = Item.find(params[:item_id])
     @buying = Buying.new(buying_params)
+    @item_price = @item.price
     if @buying.valid?
+      pay_item
       @buying.save
       redirect_to root_path
     else
@@ -20,6 +22,15 @@ class BuyingLogsController < ApplicationController
 
   private
   def buying_params
-    params.require(:buying).permit(:postal_code, :area_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:buying).permit(:postal_code, :area_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]    
+    Payjp::Charge.create(
+      amount: @item_price,  # 商品の値段
+      card: buying_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/buying.rb
+++ app/models/buying.rb
@@ -1,12 +1,13 @@
 class Buying
   include ActiveModel::Model
-  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :area_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code,    format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
     validates :city
     validates :address
     validates :phone_number,  format: { with: /\A\d{10,11}\z/, message: 'はハイフン(-)なしで入力してください'}
+    validates :token
   end
 
   validates :area_id,  numericality: { other_than: 0, message: "can't be blank" }
