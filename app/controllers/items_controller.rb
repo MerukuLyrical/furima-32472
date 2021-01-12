dc557b6659095356f2cdd08aceae3137fd72bd14
MerukuLyrical <merukurip@gmail.com>
2021-01-12 11:48:54 +0900

    全体修正版1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,6 +50,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    redirect_to root_path if current_user.id != @item.user_id && @item.buying_log.present?
+    redirect_to root_path if  current_user.id != @item.user_id || @item.buying_log.present?
   end
 end
